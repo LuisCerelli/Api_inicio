8cfeb3b683c4e698a0d4d9376dd4c031e38ca92f
LuisCerelli <cerluisalberto@gmail.com>
2024-09-14 14:02:16 +0200

    update

--- main.py
+++ main.py
@@ -5,4 +5,21 @@ import json
 url = 'https://rickandmortyapi.com/api/character/2'
 r = requests.get(url)
 response = r.text
-print(response)
+
+#ahora hay que parsearlo para ver el status :
+
+j=r.json()
+
+status = j['status']
+
+
+i = 1
+
+while i < 11:
+    url= 'https://rickandmortyapi.com/api/character/{}'.format(i)
+    r = requests.get(url)
+    j = r.json()
+    name = j['name']
+    status = j['status']
+    print('El personaje {} tiene status:  {}'.format(name, status))
+    i += 1
